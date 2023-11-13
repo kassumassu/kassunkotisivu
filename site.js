eec955995d9cd53976ed9124babefa69253af276
kassumassu <142390041+kassumassu@users.noreply.github.com>
2023-11-13 20:16:06 +0200

    Add files via upload

---
+++ site.js
@@ -0,0 +1,7 @@
+alert("tervetuloa!") 
+function s(){
+    alert("olen mielestäni taitava monissa eri tieto tekniikkaan liittyvissä asioissa") 
+} 
+function d(){
+    alert("networkchuck thio joe bandi nova mauton lakko paqpa ja glyffi") 
+}
